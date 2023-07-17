a5394d99c1a7b887d251ca196170eb9155a462e0
MoizesFerreir <moizesrafael@oultook.com>
2023-07-16 22:25:41 -0300

    First commit

---
+++ Menu.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace EditorHtml{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            DrawScreen();
+        }
+        public static void DrawScreen()
+        {
+            
+            Console.Write("+");
+            for(int i = 0; i<=30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
+
+            for(int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for(int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+             Console.Write("+");
+            for(int i = 0; i<=30; i++)
+                Console.Write("-");
+            Console.Write("+");
+            Console.Write("\n");
+        }
+    }
+}
